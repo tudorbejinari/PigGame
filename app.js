6f87f6b0f5f61bf1acc1da2975b5a0c2be0fbf89
Tudor Bejinari <tudorbejinari@gmail.com>
2019-08-18 12:49:44 -0600

    add winning score

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
         //check if player won the game
-        if(scores[activePlayer] >= 20) {
+        if(scores[activePlayer] >= 21) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
